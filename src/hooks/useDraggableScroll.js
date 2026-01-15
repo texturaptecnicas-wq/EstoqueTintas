1b59261c313fadc1ef2eb531f2b05892ae7460c2
Lucas Rheuel <lucasrheuel@gmail.com>
2026-01-15 15:48:40 -0300

    Atualização do site a partir do Horizons

--- src/hooks/useDraggableScroll.js
+++
@@ -1,5 +0,0 @@
-
-// This hook is no longer used, clearing content to keep codebase clean
-export const useDraggableScroll = () => {
-  return {};
-};
--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-
-// Supabase integration has been removed as per user request.
-// This file is kept to avoid import errors if it's referenced elsewhere, 
-// though all references should have been removed.
-
-export const supabase = null;
